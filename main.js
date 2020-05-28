ec75008ba6cebd954ecea722888dd097ed39a6e1
joinichi8 <joinichi8@gmail.com>
2020-05-28 14:42:24 +0300

    added buy option for cursor

--- main.js
+++ main.js
@@ -2,5 +2,19 @@ var cookies = 0;
 
 function cookieClick(number) {
   cookies = cookies + number;
-  document.getElementById("cookies").innerHTML = cookies;
+  document.getElementById('cookies').innerHTML = cookies;
+}
+
+var cursors = 0;
+
+function buyCursor() {
+  var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));
+  if(cookies >= cursorCost) {
+    cursors = cursors + 1;
+    cookies = cookies - cursorCost;
+    document.getElementById('cursors').innerHTML = cursors;
+    document.getElementById('cookies').innerHTML = cookies;
+  }
+  var nextCost = Math.floor(10 * Math.pow(1.1,cursors));
+  document.getElementById('cursorCost').innerHTML = nextCost;
 }
